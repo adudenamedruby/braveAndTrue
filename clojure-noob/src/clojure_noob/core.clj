0f30b7859f99798173282c8de5d208560227cdf7
roux g. buciu <github@adudenamedruby.com>
2024-10-24 18:08:21 -0400

    working

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -1,50 +1,40 @@
 (ns clojure-noob.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "I'm a little teapot."))
+(+ 1 1 3)
 
-(+ 1 2 3)
+(str "It was the panda " "in the library " "with the dust bunny.")
 
-(str "it was the panda " "with the dustbuster " "in the library!")
+;; (if boolean-form
+     ; then-form
+     ; optional-else-form)
 
 (if true
-  (do (println "success!")
-      "by zeus's hammen!")
-  (do (println "failure!")
-      "by aquaman's trident"))
+  ;; do lets you do multiple things in an if form
+  (do (println "first pass")
+      (println "first pass")
+      "By Zeus' hammer")
+  "By Aquaman's trident")
 
 (when true
-  (println "Success!")
+  (println "one")
+  (println "two")
   "abra cadabra")
 
 (nil? 1)
+(nil? nil)
 
 (or false nil :large_I_mean_venti :why_cant_I_just_say_large)
-(or (= 0 1) (= "yes" "no"))
 
-; (def failed-protagonist-names
-;   ["larry potter" "doreen the explorer" "the incredible bulk"])
+(and :free_wifi :hot_coffee)
+(and :feeling_good nil false)
 
-(def wookieName "Chewbacca")
-(str "\"Uggllglglglglglglll\" -" wookieName)
+(def severity :mild)
+(defn error-message
+  [severity]
+  (str "Oh GOD! It's a disaster! We're "
+       (if (= severity :mild)
+         "mildly incovenienced"
+         "DOOOOOOMMMMEEEEEDDDD!!!!")))
 
-(defn too-enthusiastic
-  "Return a cheer that might be too enthusiastic"
-  [name]
-  (str "Oh. My. God" name "You are most definitely like the best"))
-(too-enthusiastic "zelda")
-
-(defn x-chop
-  "Describe the kind of chop you're inflicting on someone"
-  ([name chop-type]
-   (str "I " chop-type " chop " name "! Take that!"))
-  ([name]
-   (x-chop name "karate")))
-(x-chop "Kanye West" "slap")
-(x-chop "Kanye East")
-
-(map (fn [name] (str "Hi " name))
-     ["Darth Vader" "Mr. Magooo"])
+(error-message severity)
--- hobbit-smack/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to hobbit-smack
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
